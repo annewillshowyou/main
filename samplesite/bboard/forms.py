2e3342955e109a13ec6ffa44229c9e0f206dbd1c
anne <workokidoki@gmail.com>
2024-02-16 18:08:33 +0300

    Added “Category not selected” to the category selection field, and also wrote a system for displaying errors when filling out forms

--- samplesite/bboard/forms.py
+++ samplesite/bboard/forms.py
@@ -3,6 +3,10 @@ from django.forms import ModelForm
 from .models import Bb
 
 class BbForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['rubric'].empty_label = "Категория не выбрана"
+        
     class Meta:
         model = Bb
         fields = ('title', 'content', 'price', 'rubric')
--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,18 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+# Выводит ошибку, когда она связана не с полем, а с целой моделью
+from django.core.exceptions import NON_FIELD_ERRORS
 
+# Модель объявлений
 class Bb(models.Model):
-    title = models.CharField(max_length = 50, verbose_name = "Название")
+    title = models.CharField(max_length = 50, verbose_name = "Товар")
     content = models.TextField(blank = True, null = True, verbose_name = "Описание")
     price = models.FloatField(null = True, blank = True, verbose_name = "Цена")
     published = models.DateTimeField(auto_now_add = True, db_index = True)
 
     rubric = models.ForeignKey('Rubric', null = True, on_delete = models.PROTECT, verbose_name = 'Рубрика')
 
+# Описываю, как будет вести себя форма при ошибках ввода
+    def clean(self):
+        errors = {}
+        
+        if not self.content:
+            errors['content'] = ValidationError('Укажите описание продаваемого товара')
+
+        if self.price and self.price < 0:
+            errors['price'] = ValidationError("Укажите неотрицательное значение цены")
+
+        if errors:
+            raise ValidationError(errors)
+        
+        errors[NON_FIELD_ERRORS] = ValidationError('Ошибка в модели!')
+        
+# Дополнительные параметры модели
     class Meta:
         verbose_name_plural = "Объявления"
         verbose_name = "Обьявление"
         ordering = ['-published']
+
+    def clean(self):
+        errors = {}
+        if not self.content:
+            errors['content'] = ValidationError("Укажите описание продаваемого товара")
 
 class Rubric(models.Model):
     name = models.CharField(max_length = 20, db_index = True, verbose_name = 'Название')
@@ -35,3 +60,4 @@ class AdvUser(models.Model):
 
     def __str__(self):
         return str(self.user)
+    
