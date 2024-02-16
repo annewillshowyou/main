bc6addd0aca636d295e61e556636b7401857f411
anne <workokidoki@gmail.com>
2024-02-16 14:25:54 +0300

    First commit

---
+++ samplesite/bboard/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Bb(models.Model):
+    title = models.CharField(max_length = 50, verbose_name = "Название")
+    content = models.TextField(blank = True, null = True, verbose_name = "Описание")
+    price = models.FloatField(null = True, blank = True, verbose_name = "Цена")
+    published = models.DateTimeField(auto_now_add = True, db_index = True)
+
+    rubric = models.ForeignKey('Rubric', null = True, on_delete = models.PROTECT, verbose_name = 'Рубрика')
+
+    class Meta:
+        verbose_name_plural = "Объявления"
+        verbose_name = "Обьявление"
+        ordering = ['-published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length = 20, db_index = True, verbose_name = 'Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Рубрики"
+        verbose_name = "Рубрика"
+        ordering = ['name']
+
+class AdvUser(models.Model):
+    is_activated = models.BooleanField(default = True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE, verbose_name = "Пользователи")
+
+    class Meta:
+        verbose_name_plural = "Пользователи"
+        verbose_name = "Пользователь"
+
+    def __str__(self):
+        return str(self.user)
